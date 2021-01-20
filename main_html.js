1ad4751549a5d69f438ef8b8097b6fdd788658b5
Pam Johnston <1609098@rgu.ac.uk>
2021-01-20 12:40:35 +0000

    fixing ports again....

--- main_html.js
+++ main_html.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
   fs.createReadStream('myWebPage.html').pipe(res)
 })
 
-server.listen(process.env.PORT || 80)
-console.log('Server running at http://127.0.0.1:80/');
+server.listen(process.env.PORT || 8000)
+console.log('Server running at http://127.0.0.1:8000/');
